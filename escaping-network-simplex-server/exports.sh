3b112efa532fd75923a756f532c6cf08282f9000
Leo <leandro.martin@protonmail.com>
2024-09-21 13:57:15 -0300

    expose ports

--- escaping-network-simplex-server/exports.sh
+++ escaping-network-simplex-server/exports.sh
@@ -1,5 +1,5 @@
 export APP_SIMPLEX_SERVER_SMP_PORT="5223"
-export APP_SIMPLEX_SERVER_XFTP_PORT="443"
+export APP_SIMPLEX_SERVER_XFTP_PORT="5443"
 
 local app_simplex_server_smp_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-smp/hostname"
 local app_simplex_server_xftp_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-xftp/hostname"
